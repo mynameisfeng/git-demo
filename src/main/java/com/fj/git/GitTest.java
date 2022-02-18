635340585675350f4447e65473a0e97a5296bf21
fj <1475547954@qq.com>
2022-02-18 21:17:08 +0800

    created gitTest class

---
+++ src/main/java/com/fj/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.fj.git;
+
+/**
+ * @author Mr冯
+ * @date 2022/2/18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("first commit!");
+    }
+}
