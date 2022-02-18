dac7c37d63dc5faef857f7c5c8ae745fbbdd8a7f
fj <1475547954@qq.com>
2022-02-18 22:02:06 +0800

    hot-fix first commit

--- src/main/java/com/fj/git/GitTest.java
+++ src/main/java/com/fj/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("second commit!");
         System.out.println(" test !");
+        System.out.println("current branch hot-fix");
     }
 }
